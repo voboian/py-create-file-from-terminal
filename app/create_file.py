340c82a47919f366ad860553463d082f7eece57e
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-06-10 14:18:18 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,69 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def create_file(file_name: str, path_parts: list) -> None:
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    content = date_time
+    line_number = 1
+    while True:
+        message = input("Enter content line: ")
+        if message == "stop":
+            break
+        else:
+            content += f"\n{str(line_number)} {message}"
+            line_number += 1
+
+    parent_path = get_path(path_parts)
+    file_path = os.path.join(parent_path, file_name)
+    is_exist = os.path.exists(file_path)
+
+    with open(file_path, "a") as file:
+        if is_exist:
+            file.write(f"\n\n")
+        file.write(f"{content}")
+
+
+def create_dir(path_parts: list) -> None:
+    parent_path = get_path(path_parts)
+    if not os.path.exists(parent_path):
+        os.makedirs(parent_path)
+
+
+def get_path(path_parts: list) -> str:
+    parent_path: str = os.getcwd()
+    for dir in path_parts:
+        parent_path = os.path.join(parent_path, dir)
+    return parent_path
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    if len(args) == 0:
+        return
+    if "-d" in args and "-f" in args:
+        dir_index = args.index("-d")
+        file_index = args.index("-f")
+
+        path_parts = args[dir_index + 1:file_index]
+        file_name = args[file_index + 1]
+
+        create_dir(path_parts)
+        create_file(file_name, path_parts)
+
+    elif "-d" in args:
+        dir_index = args.index("-d")
+        path_parts = args[dir_index + 1:]
+
+        create_dir(path_parts)
+
+    elif "-f" in args:
+        dir_index = args.index("-f")
+        file_name = args[dir_index + 1]
+
+        create_file(file_name, [])
+
+
+if __name__ == "__main__":
+    main()
